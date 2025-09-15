79e3d11ef403f0cf52b7b8dd910710efd2d4e636
fede <federicolun@gmail.com>
2025-09-15 13:43:57 -0300

    testing

--- api/charge.js
+++ api/charge.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     try {
         const {
             amountUsd = 100, // monto base en USD
-            fee = { type: "percent", value: 0.5 }
+            fee = { type: "percent", value: 0.25 }
         } = req.body || {};
 
         // 🔥 Obtener precio MATIC/USD de CoinGecko
